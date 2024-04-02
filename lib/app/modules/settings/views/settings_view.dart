0493d7218cb0307ab6e7d2c7865f1fc8f93e9a45
azzouzin <azzouzmerw@gmail.com>
2024-04-02 11:19:27 +0200

    Add url_launcher plugin for launching URLs and emails in SettingsView
    - Added url_launcher plugin to enable launching URLs and emails in the SettingsView
    - Implemented functions to handle launching emails and phone numbers
    - Updated pubspec.yaml to include url_launcher plugin version 6.2.5

--- lib/app/modules/settings/views/settings_view.dart
+++ lib/app/modules/settings/views/settings_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:getx_skeleton/app/components/custom_snackbar.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:get/get.dart';
 import 'package:getx_skeleton/app/modules/login/login_controller.dart';
 import 'package:getx_skeleton/config/translations/localization_service.dart';
@@ -61,12 +62,40 @@ class SettingsView extends GetView<SettingsController> {
                 onTap: () {
                   LocalizationService.updateLanguage(
                       Get.locale!.languageCode == "en" ? "ar" : "en");
-                  ;
                 }),
             25.verticalSpace,
             SettingsItem(
               title: Strings.contactus.tr,
               icon: Constants.helpIcon,
+              onTap: () {
+                showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          insetPadding: EdgeInsets.all(5),
+                          contentPadding: EdgeInsets.all(8),
+                          title: Text(Strings.contactus.tr),
+                          content: Container(
+                            width: Get.width,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                callRow("0550027733", () {
+                                  lunchNumber("0550027733");
+                                }, Icons.call),
+                                callRow("0550027732", () {
+                                  lunchNumber("0550027732");
+                                }, Icons.call),
+                                callRow("0770803003", () {
+                                  lunchNumber("0770803003");
+                                }, Icons.call),
+                                callRow("sarl.iampacking@gmail.com", () {
+                                  lunchemail("sarl.iampacking@gmail.com");
+                                }, Icons.email),
+                              ],
+                            ),
+                          ),
+                        ));
+              },
             ),
             25.verticalSpace,
             SettingsItem(
@@ -79,5 +108,57 @@ class SettingsView extends GetView<SettingsController> {
         ),
       ),
     );
+  }
+
+  Padding callRow(String number, Function() onTap, IconData iconData) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            number,
+            style: Get.textTheme.bodyLarge,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(shape: CircleBorder()),
+            onPressed: () {
+              onTap();
+            },
+            child: Icon(
+              iconData,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Future<void> lunchemail(String emailtitle) async {
+    String email = Uri.encodeComponent(emailtitle);
+    String subject = Uri.encodeComponent("Client Support");
+    String url = "mailto:$email?subject=$subject";
+
+    try {
+      await launch(url);
+    } catch (e) {
+      CustomSnackBar.showCustomErrorSnackBar(
+        title: "Can't send email",
+        message: e.toString(),
+      );
+    }
+  }
+
+  Future<void> lunchNumber(String number) async {
+    String url = "tel:$number";
+    try {
+      await launch(url);
+    } catch (e) {
+      CustomSnackBar.showCustomErrorSnackBar(
+        title: "Can't call this number",
+        message: e.toString(),
+      );
+    }
   }
 }
